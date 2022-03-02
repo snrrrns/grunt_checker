adf0422b3f9769b8431cebf095c6ba67ca11abfb
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-03-02 23:39:27 +0900

    Chore: results#createへのルーティングを追加 #20

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#top'
 
   resources :recordings, only: %i[index show] do
-    resources :results, param: :uuid, only: %i[show]
+    resources :results, param: :uuid, only: %i[show create]
   end
 end
