2d93fdbf562f0464ff703627c40839b333766826
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-03-02 23:50:10 +0900

    Feat: recordingsの一覧・詳細の仮ページを作成 #20

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -1,5 +1,15 @@
 class RecordingsController < ApplicationController
-  def index; end
+  before_action :set_recording, only: %i[show]
+
+  def index
+    @recordings = Recording.all
+  end
 
   def show; end
+
+  private
+
+  def set_recording
+    @recording = Recording.find(params[:id])
+  end
 end
