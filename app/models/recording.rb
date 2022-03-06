d32ca08e6b543a16d2906f68adf46ba7f1e6f682
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-03-07 00:47:01 +0900

    Remove: instrumentsとexample_vocalのユニーク制約を削除 #28
    CarrierWaveによるアップロードの妨げになる為。

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -11,14 +11,10 @@
 #
 # Indexes
 #
-#  index_recordings_on_example_vocal  (example_vocal) UNIQUE
-#  index_recordings_on_instruments    (instruments) UNIQUE
-#  index_recordings_on_vocal_style    (vocal_style) UNIQUE
+#  index_recordings_on_vocal_style  (vocal_style) UNIQUE
 #
 class Recording < ApplicationRecord
   has_many :results, dependent: :delete_all
 
   validates :vocal_style, presence: true, uniqueness: true
-  validates :example_vocal, uniqueness: true, allow_blank: true
-  validates :instruments, uniqueness: true, allow_blank: true
 end
