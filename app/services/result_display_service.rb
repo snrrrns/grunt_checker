846b1f2d60026fe8d5f72a1eba982097742c8d72
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-03-21 22:11:33 +0900

    Style: アプリ全体のデザイン・文言を作り込み #50

--- app/services/result_display_service.rb
+++ app/services/result_display_service.rb
@@ -7,37 +7,44 @@ class ResultDisplayService
     if @result.score.ceil(0).zero?
       {
         score: (@result.created_at.strftime('%S')[1].to_i).ceil(0),
-        comment: 'Keep trying!!'
+        evaluation: 'Keep trying!',
+        comment: 'お手本を聴いて再チャレンジ！'
       }
     elsif @result.score.ceil(0) > 0 && @result.score.ceil(0) < 20
       {
         score: @result.score.ceil(0),
-        comment: 'Keep trying!!'
+        evaluation: 'Keep trying!',
+        comment: 'お手本を聴いて再チャレンジ！'
       }
     elsif @result.score.ceil(0) >= 20 && @result.score.ceil(0) < 40
       {
         score: @result.score.ceil(0),
-        comment: 'OK!!'
+        evaluation: 'OK!',
+        comment: 'もっといけそうです！更に練習してみよう！'
       }
     elsif @result.score.ceil(0) >= 40 && @result.score.ceil(0) < 60
       {
         score: @result.score.ceil(0),
-        comment: 'Nice!!'
+        evaluation: 'Nice!!',
+        comment: 'いい感じです！更に磨きをかけよう！'
       }
     elsif @result.score.ceil(0) >= 60 && @result.score.ceil(0) < 80
       {
         score: @result.score.ceil(0),
-        comment: 'Great!!'
+        evaluation: 'Great!!!',
+        comment: 'かなりいい線！バンドのコーラスは担当できそうです！'
       }
     elsif @result.score.ceil(0) >= 80 && @result.score.ceil(0) < 90
       {
         score: @result.score.ceil(0),
-        comment: 'Exellent!!'
+        evaluation: 'Exellent!!!!',
+        comment:'すばらしい！デスボイスでボーカルやれちゃいます！'
       }
     else
       {
         score: @result.score.ceil(0),
-        comment: 'Insane!!'
+        evaluation: 'Insane!!!!!',
+        comment: '本業の方ですか？さすがです！'
       }
     end
   end
