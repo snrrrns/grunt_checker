1ab1d05375957693aba3fb9461f0ccd2eebd5288
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-03-23 19:15:25 +0900

    Feat: 利用規約を実装 #46

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
+  get '/terms', to: 'static_pages#terms'
 
   resources :recordings, only: %i[index show edit update] do
     resources :results, param: :uuid, only: %i[show create], shallow: true
