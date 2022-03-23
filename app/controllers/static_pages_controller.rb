8c6c07683b37a1c74a351d74e11385a1697f3a8d
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-03-23 22:31:31 +0900

    Feat: お問い合わせページを実装 #46

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,6 @@ class StaticPagesController < ApplicationController
   def terms; end
 
   def privacy_policy; end
+
+  def inquiry; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get '/terms', to: 'static_pages#terms'
   get '/privacy_policy', to: 'static_pages#privacy_policy'
+  get '/inquiry', to: 'static_pages#inquiry'
 
   resources :recordings, only: %i[index show edit update] do
     resources :results, param: :uuid, only: %i[show create], shallow: true
