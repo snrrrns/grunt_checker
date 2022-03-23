491ca1c0eccc48380646faa71738f27b4b0e5fc0
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-03-23 21:36:25 +0900

    Feat: プライバシーポリシーを実装 #46

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   get '/terms', to: 'static_pages#terms'
+  get '/privacy_policy', to: 'static_pages#privacy_policy'
 
   resources :recordings, only: %i[index show edit update] do
     resources :results, param: :uuid, only: %i[show create], shallow: true
