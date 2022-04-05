5bd5f1f5f2b1023747151741a4915bccde80e50b
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-04-05 12:20:38 +0900

    Feat: meta-tagsを利用したヘルパーメソッドを使用 #86
    ヘルパーメソッドpage_titleとcontent_forの記述は不要になった為削除。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,27 @@
 module ApplicationHelper
-  def page_title(page_title = '')
-    base_title = 'Grunt Checker - デスボイス測定アプリ'
+  def default_meta_tags
+    {
+      site: 'Grunt Checker - デスボイス測定アプリ',
+      title: title,
+      reverse: true,
+      description: 'あなたのデスボイスを測定し、バンドの演奏と融合します！',
+      canonical: 'https://www.gruntchecker.com',
+      noindex: !Rails.env.production?,
+      icon: image_url('favicon.ico'),
 
-    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+      og: {
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: 'https://www.gruntchecker.com',
+        image: image_url('ogp.png'),
+        locale: 'ja_JP'
+      },
+
+      twitter: {
+        card: 'summary_large_image',
+        creator: '@snrrrns'
+      }
+    }
   end
 end
