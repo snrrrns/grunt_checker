3006ef17cf8ea33d479bab68bd35f4e572be31c2
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-04-21 13:17:40 +0900

    Remove: RecordingsControllerの不要なコードを削除 #112

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -5,22 +5,9 @@ class RecordingsController < ApplicationController
     @recordings = Recording.all.order(created_at: :asc)
   end
 
-  def create
-    @recording = Recording.new(recording_params)
-    if @recording.save
-      redirect_to recordings_path
-    else
-      render :new
-    end
-  end
-
   def show; end
 
   private
-
-  def recording_params
-    params.require(:recording).permit(:vocal_style, :emotion, :summary, :vocal_image, :example_vocal, :embed_identifier)
-  end
 
   def set_recording
     @recording = Recording.find(params[:id])
