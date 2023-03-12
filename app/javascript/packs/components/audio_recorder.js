9b12b84fed6ffc949d3131195e79f590f746ebf0
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2023-03-12 19:38:21 +0900

    Refactor: 音声の録音処理をクラス化 #120

---
+++ app/javascript/packs/components/audio_recorder.js
@@ -0,0 +1,167 @@
+import { audioPlayer, permissionButton, recordButton, stopButton, downloadLink, audioOrigin } from "../utils/recording_dom_elements";
+
+const BUFFER_SIZE = 1024;
+
+export default class AudioRecorder {
+  constructor(audioVisualizer, message) {
+    this.audioData = [];
+    this.stream = null;
+    this.audioContext = null;
+    this.audioSampleRate = null;
+    this.micBlobUrl = null;
+    this.scriptProcessor = null;
+    this.mediaStreamSource = null;
+    this.recStart = null;
+    this.countdownTime = null;
+    this.timeout = null;
+    this.audioVisualizer = audioVisualizer;
+    this.message = message;
+  }
+
+  init() {
+    audioPlayer.src = '';
+    this.message.standby();
+    if (!this.stream) {
+      navigator.mediaDevices.getUserMedia({
+        video: false,
+        audio: {
+          echoCancellation: true,
+          echoCancellationType: 'system',
+          noiseSuppression: false,
+        }
+      })
+        .then((audio) => {
+          permissionButton.classList.add('d-none');
+          recordButton.classList.remove('d-none');
+          this.stream = audio;
+          this.audioVisualizer.startVisualization(this.stream);
+          return this.stream;
+        })
+        .catch((error) => {
+          console.error('mediaDevices.getUserMedia() error:', error);
+        });
+    }
+  }
+
+  startRecording() {
+    this.recStart = setTimeout(() => {
+      recordButton.classList.add('d-none');
+      stopButton.classList.remove('d-none');
+      this.audioData = [];
+      this.audioContext = new AudioContext();
+      this.audioSampleRate = this.audioContext.sampleRate;
+      this.scriptProcessor = this.audioContext.createScriptProcessor(BUFFER_SIZE, 2, 2);
+      this.mediaStreamSource = this.audioContext.createMediaStreamSource(this.stream);
+      this.mediaStreamSource.connect(this.scriptProcessor);
+      this.scriptProcessor.onaudioprocess = this.onAudioProcess.bind(this);
+      this.scriptProcessor.connect(this.audioContext.destination);
+      this.message.recordingStart();
+      let remainingTime = 4;
+      this.countdownTime = setInterval(() => {
+        let sec = remainingTime--;
+        this.message.recordingInProgress(sec);
+        if (sec === 0) {
+          clearInterval(this.countdownTime);
+        }
+      }, 1000);
+      this.timeout = setTimeout(() => {
+        this.stopRecording();
+      }, 5000);
+      stopButton.addEventListener('click', this.stopRecording.bind(this));
+    }, 2000);
+  }
+
+  onAudioProcess(e) {
+    const input = e.inputBuffer.getChannelData(0);
+    const bufferData = new Float32Array(BUFFER_SIZE);
+    for (let i = 0; i < BUFFER_SIZE; i++) {
+      bufferData[i] = input[i];
+    }
+    this.audioData.push(bufferData);
+  }
+
+  stopRecording() {
+    stopButton.click();
+    clearInterval(this.countdownTime);
+    clearTimeout(this.timeout);
+    clearTimeout(this.recStart);
+    this.saveAudio(this.audioData);
+    this.message.completeRecording();
+  }
+
+  saveAudio(audioData) {
+    this.exportWav(audioData);
+    downloadLink.download = 'recorded.wav';
+    this.audioContext.close().then(function() {
+    });
+  }
+
+  exportWav(audioData) {
+    const dataView = this.encodeWav(this.mergeBuffers(audioData), this.audioSampleRate);
+    const audioBlob = new Blob([dataView], { type: 'audio/wav' });
+    this.micBlobUrl = window.URL.createObjectURL(audioBlob);
+    const myUrl = window.URL || window.webkitURL;
+    const url = myUrl.createObjectURL(audioBlob);
+    downloadLink.href = url;
+    audioOrigin.src = url;
+
+  }
+
+  encodeWav(samples, sampleRate) {
+    const buffer = new ArrayBuffer(44 + samples.length * 2);
+    const dataView = new DataView(buffer);
+    const writeStringToDataView = (dataView, offset, formatStr) => {
+      for (let i = 0; i < formatStr.length; i++) {
+        dataView.setUint8(offset + i, formatStr.charCodeAt(i));
+      }
+    };
+    const floatTo16BitPCM = (pcm16BitData, offset, samples) => {
+      for (let i = 0; i < samples.length; i++, offset += 2) {
+        let sample = Math.max(-1, Math.min(1, samples[i]));
+        pcm16BitData.setInt16(offset, sample < 0 ? sample * 0x8000 : sample * 0x7FFF, true);
+      }
+    };
+    writeStringToDataView(dataView, 0, 'RIFF');
+    dataView.setUint32(4, 32 + samples.length * 2, true);
+    writeStringToDataView(dataView, 8, 'WAVE');
+    writeStringToDataView(dataView, 12, 'fmt ');
+    dataView.setUint32(16, 16, true);
+    dataView.setUint16(20, 1, true);
+    dataView.setUint16(22, 1, true);
+    dataView.setUint32(24, sampleRate, true);
+    dataView.setUint32(28, sampleRate * 2, true);
+    dataView.setUint16(32, 2, true);
+    dataView.setUint16(34, 16, true);
+    writeStringToDataView(dataView, 36, 'data');
+    dataView.setUint32(40, samples.length * 2, true);
+    floatTo16BitPCM(dataView, 44, samples);
+    return dataView;
+  }
+
+  mergeBuffers(audioData) {
+    let sampleLength = 0;
+    let sampleIdx = 0;
+    for (let i = 0; i < audioData.length; i++) {
+      sampleLength += audioData[i].length;
+    }
+    const samples = new Float32Array(sampleLength);
+    for (let i = 0; i < audioData.length; i++) {
+      for (let j = 0; j < audioData[i].length; j++) {
+        samples[sampleIdx] = audioData[i][j];
+        sampleIdx++;
+      }
+    }
+    return samples
+  };
+
+  playBack() {
+    if (this.micBlobUrl) {
+      audioPlayer.src = this.micBlobUrl;
+      audioPlayer.onended = () => {
+        audioPlayer.pause();
+        audioPlayer.src = '';
+      };
+      audioPlayer.play();
+    }
+  }
+}
--- app/javascript/packs/recording.js
+++ app/javascript/packs/recording.js
@@ -1,6 +1,7 @@
 import CountIn from './components/count_in'
 import Message from './components/massage'
 import AudioVisualizer from "./components/audio_visualizer";
+import AudioRecorder from "./components/audio_recorder";
 import element from "./utils/recording_dom_elements";
 import axios from 'axios';
 axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest';
@@ -8,20 +9,10 @@ axios.defaults.headers['X-CSRF-TOKEN'] = document.getElementsByName('csrf-token'
 
 const message = new Message();
 const audioVisualizer = new AudioVisualizer();
+const audioRecorder = new AudioRecorder(audioVisualizer, message);
 
-const BUFFER_SIZE = 1024;
 const GAIN_VALUE = 0.9;
 
-let audioData = [];
-let stream = null;
-let audioContext = null;
-let audioSampleRate = null;
-let micBlobUrl = null;
-let scriptProcessor = null;
-let mediaStreamSource = null;
-let recStart = null;
-let countDownTime = null;
-let timeout = null;
 let mixContext = null;
 let mixDestination = null;
 let srcInstruments = null;
@@ -30,85 +21,6 @@ let mixRecorder = null;
 let mixData = [];
 let mixUrl = null;
 let gainNode = null;
-
-
-function onAudioProcess(e) {
-  const input = e.inputBuffer.getChannelData(0);
-  const bufferData = new Float32Array(BUFFER_SIZE);
-  for (let i = 0; i < BUFFER_SIZE; i++) {
-    bufferData[i] = input[i];
-  }
-  audioData.push(bufferData);
-}
-
-function saveAudio() {
-  exportWAV(audioData);
-  element.downloadLink.download = 'recorded.wav';
-  audioContext.close().then(function() {
-  });
-}
-
-function exportWAV(audioData) {
-  const encodeWAV = (samples, sampleRate) => {
-    const buffer = new ArrayBuffer(44 + samples.length * 2);
-    const view = new DataView(buffer);
-
-    const writeString = (view, offset, string) => {
-      for(let i = 0; i < string.length; i++) {
-        view.setUint8(offset + i, string.charCodeAt(i));
-      }
-    };
-
-    const floatTo16BitPCM = (output, offset, input) => {
-      for(let i = 0; i < input.length; i++, offset += 2) {
-        let s = Math.max(-1, Math.min(1, input[i]));
-        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
-      }
-    };
-
-    writeString(view, 0, 'RIFF');
-    view.setUint32(4, 32 + samples.length * 2, true);
-    writeString(view, 8, 'WAVE');
-    writeString(view, 12, 'fmt ');
-    view.setUint32(16, 16, true);
-    view.setUint16(20, 1, true);
-    view.setUint16(22, 1, true);
-    view.setUint32(24, sampleRate, true);
-    view.setUint32(28, sampleRate * 2, true);
-    view.setUint16(32, 2, true); 
-    view.setUint16(34, 16, true);
-    writeString(view, 36, 'data');
-    view.setUint32(40, samples.length * 2, true);
-    floatTo16BitPCM(view, 44, samples);
-    return view;
-  };
-
-  const mergeBuffers = (audioData) => {
-    let sampleLength = 0;
-    for(let i = 0; i < audioData.length; i++) {
-      sampleLength += audioData[i].length;
-    }
-    const samples = new Float32Array(sampleLength);
-    let sampleIdx = 0;
-    for(let i = 0; i < audioData.length; i++) {
-      for(let j = 0; j < audioData[i].length; j++) {
-        samples[sampleIdx] = audioData[i][j];
-        sampleIdx++;
-      }
-    }
-    return samples;
-  };
-
-  const dataview = encodeWAV(mergeBuffers(audioData), audioSampleRate);
-  const audioBlob = new Blob([dataview], { type: 'audio/wav' });
-  micBlobUrl = window.URL.createObjectURL(audioBlob);
-  console.log(dataview);
-
-  const myURL = window.URL || window.webkitURL;
-  const url = myURL.createObjectURL(audioBlob);
-  element.downloadLink.href = url;
-  element.audioOrigin.src = url;
-}
 
 async function mixing() {
   console.log('ミックス開始');
@@ -206,32 +118,7 @@ window.addEventListener('resize', () => {
 audioVisualizer.resize();
 
 element.permissionButton.onclick = function() {
-  element.audioPlayer.src = '';
-  if(!stream) {
-    navigator.mediaDevices.getUserMedia({
-      video: false,
-      audio: {
-        echoCancellation: true,
-        echoCancellationType: 'system',
-        noiseSuppression: false
-      }
-    })
-      .then(function(audio) {
-        element.permissionButton.classList.add('d-none');
-        element.recordButton.classList.remove('d-none');
-
-        stream = audio;
-        audioVisualizer.startVisualization(stream);
-        console.log('録音可能です');
-        return stream;
-      })
-
-      .catch(function(error) {
-        console.error('mediaDevide.getUserMedia() error:', error);
-        return;
-      });
-  }
-
+  audioRecorder.init();
   message.standby();
   element.permissionButton.disabled = true;
   element.recordButton.disabled = false;
@@ -251,42 +138,7 @@ element.recordButton.onclick = function() {
   message.setReady();
   const countIn = new CountIn();
   countIn.start();
-
-  recStart = setTimeout(() => {
-    element.recordButton.classList.add('d-none');
-    element.stopButton.classList.remove('d-none');
-
-    audioData = [];
-    audioContext = new AudioContext();
-    audioSampleRate = audioContext.sampleRate;
-    scriptProcessor = audioContext.createScriptProcessor(BUFFER_SIZE, 2, 2);
-    mediaStreamSource = audioContext.createMediaStreamSource(stream);
-    mediaStreamSource.connect(scriptProcessor);
-    scriptProcessor.onaudioprocess = onAudioProcess;
-    scriptProcessor.connect(audioContext.destination);
-
-    message.recordingStart();
-    let remainingTime = 4;
-    countDownTime = setInterval(() => {
-      let sec = remainingTime--;
-      message.recordingInProgress(sec);
-      if (sec === 0) {
-        clearInterval(countDownTime);
-      }
-    }, 1000);
-
-    timeout = setTimeout(() => {
-      element.stopButton.click();
-    }, 5000);
-
-    element.stopButton.addEventListener('click', () => {
-      clearInterval(countDownTime);
-      clearTimeout(timeout);
-      clearTimeout(recStart);
-      message.completeRecording();
-      console.log('停止しました');
-    });
-  }, 2000);
+  audioRecorder.startRecording();
 };
 
 element.stopButton.onclick = function() {
@@ -294,8 +146,6 @@ element.stopButton.onclick = function() {
   element.playbackButton.classList.remove('d-none');
   element.retakeButton.classList.remove('d-none');
   element.resultButton.classList.remove('d-none');
-
-  saveAudio();
   element.recordButton.disabled = false;
   element.stopButton.disabled = true;
   element.playbackButton.disabled = false;
@@ -304,14 +154,7 @@ element.stopButton.onclick = function() {
 };
 
 element.playbackButton.onclick = function() {
-  if(micBlobUrl) {
-    element.audioPlayer.src = micBlobUrl;
-    element.audioPlayer.onended = function() {
-      element.audioPlayer.pause();
-      element.audioPlayer.src = '';
-    };
-    element.audioPlayer.play();
-  }
+  audioRecorder.playBack();
 };
 
 element.exampleButton.onclick = function() {
