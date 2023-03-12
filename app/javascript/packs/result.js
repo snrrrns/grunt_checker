02894bb40d7195e2fa31651c0979e3aa666fbfed
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2023-03-12 10:49:07 +0900

    Fix: 再代入がされていない変数をletではなくconstで宣言 #120

--- app/javascript/packs/result.js
+++ app/javascript/packs/result.js
@@ -1,7 +1,6 @@
 const jsVocalPlayer = document.getElementById('js-vocal-player');
 const jsSongPlayer = document.getElementById('js-song-player');
-
-let audioVol = 0.5;
+const audioVol = 0.5;
 
 jsVocalPlayer.volume = audioVol;
 jsSongPlayer.volume = audioVol;
