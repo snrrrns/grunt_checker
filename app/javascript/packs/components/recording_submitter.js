284a9d1506b0e66ac98aa259d878bc1cfeaaf890
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2023-03-12 22:09:51 +0900

    Refactor: 音声の登録処理をクラス化 #120

---
+++ app/javascript/packs/components/recording_submitter.js
@@ -0,0 +1,48 @@
+import { downloadLink, mixLink } from '../utils/recording_dom_elements'
+import axios from 'axios';
+
+export default class RecordingSubmitter {
+  constructor() {
+    axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest';
+    axios.defaults.headers['X-CSRF-TOKEN'] = document.getElementsByName('csrf-token')[0].getAttribute('content');
+
+  }
+  submit() {
+    this.firstRequest();
+  }
+
+  firstRequest() {
+    const request = new XMLHttpRequest();
+    request.open('GET', downloadLink.href, true);
+    request.responseType = 'blob';
+    request.onload = () => {
+      const vocalBlob = request.response;
+      this.secondRequest(vocalBlob);
+    }
+    request.send();
+  }
+
+  secondRequest(vocalBlob) {
+    const request = new XMLHttpRequest();
+    request.open('GET', mixLink.href, true);
+    request.responseType = 'blob';
+    request.onload = () => {
+      const mixBlob = request.response;
+      const formData = new FormData();
+      formData.append('recording_id', document.getElementById('recording_id').value);
+      formData.append('vocal_data', vocalBlob, 'vocal.wav');
+      formData.append('compose_song', mixBlob, 'song.webm');
+      axios.post(document.getElementById('voice-form').action, formData, {
+        headers: {
+          'content-type': 'multipart/form-data',
+        }
+      }).then(response => {
+        const data = response.data;
+        window.location.href = data.url;
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+    request.send();
+  }
+}
--- app/javascript/packs/recording.js
+++ app/javascript/packs/recording.js
@@ -3,50 +3,14 @@ import Message from './components/massage'
 import AudioVisualizer from "./components/audio_visualizer";
 import AudioRecorder from "./components/audio_recorder";
 import AudioMixer from "./components/audio_mixer";
+import RecordingSubmitter from "./components/recording_submitter";
 import element from "./utils/recording_dom_elements";
-import axios from 'axios';
-axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.headers['X-CSRF-TOKEN'] = document.getElementsByName('csrf-token')[0].getAttribute('content');
 
 const message = new Message();
 const audioVisualizer = new AudioVisualizer();
 const audioRecorder = new AudioRecorder(audioVisualizer, message);
 const audioMixer = new AudioMixer();
-
-function firstXhr() {
-  const xhr1 = new XMLHttpRequest();
-  xhr1.open('GET', element.downloadLink.href, true);
-  xhr1.responseType = 'blob';
-  xhr1.send();
-  xhr1.onload = function() {
-    const vocalBlob = this.response;
-    secondXhr(vocalBlob);
-  };
-}
-
-function secondXhr(vocalBlob) {
-  const xhr2 = new XMLHttpRequest();
-  xhr2.open('GET', element.mixLink.href, true);
-  xhr2.responseType = 'blob';
-  xhr2.send();
-  xhr2.onload = function() {
-    const mixBlob = this.response;
-    const formData = new FormData();
-    formData.append('recording_id', document.getElementById('recording_id').value);
-    formData.append('vocal_data', vocalBlob, 'vocal.wav');
-    formData.append('compose_song', mixBlob, 'song.webm');
-    axios.post(document.getElementById('voiceform').action, formData, {
-      headers: {
-        'content-type': 'multipart/form-data',
-      }
-    }).then(response => {
-      const data = response.data;
-      window.location.href = data.url;
-    }).catch(error => {
-      console.log(error.responce);
-    });
-  };
-}
+const recordingSubmitter = new RecordingSubmitter();
 
 element.audioPlayer.volume = 0.5;
 element.permissionButton.disabled = false;
@@ -134,6 +98,6 @@ element.resultButton.onclick = function() {
 
   audioMixer.mixStart().then(() => {
     message.analysisResult();
-    firstXhr();
+    recordingSubmitter.submit();
   });
 };
